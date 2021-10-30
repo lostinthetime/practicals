65ee5fe8ffa2f0572c7fa0dfffb4f69e8ff76900
lostinthetime <91969098+lostinthetime@users.noreply.github.com>
2021-10-30 12:42:07 +0530

    task1b completed

--- Practical-09/task1-b.cpp
+++ Practical-09/task1-b.cpp
@@ -1,20 +1,36 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 class Student{
     private:
-    string name;
-    int year,section,marks,age;
-    Student(string name,int age,int age,int year,int section,int marks){
+    string name,section;
+    int year,marks,age;
+public:
+    Student(string name,int age,int year,string section,int marks){
         this->name = name;
         this->age = age;
         this->year = year;
         this->section = section;
         this->marks = marks;
     }
+	int getMarks(){
+        return this->marks;
+    }
 };
 
 
 int main(){
-    Student 
+    Student* students = (Student*)(malloc(sizeof(Student)*5)); 
+    string names[5] = {"Ramesh","Ganesh","Suresh","Dharmesh","Chanesh"};
+    int marks[5] = {89,87,65,45,99};    
+    string sections[5] = {"A","B","C","C","B"};
+    for(int i= 0;i< 5; i++){
+        students[i] = Student(names[i],17,2021,sections[i],marks[i]);
+    }
+    int total_marks = 0;
+    for(int i=0;i< 5;i++){
+        total_marks+= students[i].getMarks();
+    }
+    cout << "Total Marks of College " << total_marks << endl;
 }
